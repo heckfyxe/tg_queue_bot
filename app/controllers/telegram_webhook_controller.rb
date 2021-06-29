407e426ce0d57a6f2d380583e29589c37c5750f8
heckfyxe <heckfyxe@gmail.com>
2021-06-29 20:02:37 +0300

    Fixed fixed with message responses.

--- app/controllers/telegram_webhook_controller.rb
+++ app/controllers/telegram_webhook_controller.rb
@@ -1,9 +1,9 @@
 class TelegramWebhookController < Telegram::Bot::UpdatesController
   def start!(*)
-    respond_with :message, "Hi, nice to meet you!"
+    respond_with :message, text: 'Hi, nice to meet you!'
   end
 
   def hello!
-    respond_with :message, 'Hello, the Ruby World!'
+    respond_with :message, text: 'Hello, the Ruby World!'
   end
 end
