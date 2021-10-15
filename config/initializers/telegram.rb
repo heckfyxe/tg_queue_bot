de64065eff77348e5e32593d114d83242fab7eeb
heckfyxe <heckfyxe@gmail.com>
2021-10-15 05:42:02 +0300

    Fix

--- config/initializers/telegram.rb
+++ config/initializers/telegram.rb
@@ -1,5 +1,5 @@
-# require 'telegram/bot'
-#
-# Telegram.bots_config = {
-#   default: Rails.application.credentials[:telegram][:bot]
-# }
+require 'telegram/bot'
+
+Telegram.bots_config = {
+  default: Rails.application.credentials[:telegram][:bot]
+}
